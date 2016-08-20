47bdffdc935e75868ac9153d8110d1b4464a67af
Orivaldo V Santana Jr <oriva@oriva-Lenovo-G400s>
2016-08-20 11:18:33 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: oriva
+ *
+ * Created on 20 de Agosto de 2016, 10:56
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
